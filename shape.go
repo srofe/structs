4953c0563893a31c1008520ac1e6ba27336476e5
Simon Rofe <simon.rofe@ieee.org>
2022-01-06 14:51:23 +1000

    Add a Circle and use methods for Area on structs

--- shape.go
+++ shape.go
@@ -1,14 +1,24 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+type Circle struct {
+	Radius float64
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
--- shapes_test.go
+++ shapes_test.go
@@ -12,10 +12,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("The Area function shall find the area for a rectangle", func(t *testing.T) {
+	t.Run("A Rectangle shall be able to calculate its Area", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("A Rectangle shall be able to calculate its Area", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
