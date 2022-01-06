760e697e1b6e8d4e1f0acb60216e9c81e7385983
Simon Rofe <simon.rofe@ieee.org>
2022-01-06 14:41:23 +1000

    Use a sub-test

--- shapes_test.go
+++ shapes_test.go
@@ -12,10 +12,12 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("The Area function shall find the area for a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
+		want := 72.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
